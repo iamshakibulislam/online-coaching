ea0fab265a61d1c1bac28a9f5bed931b062369fd
shakibul islam <iamshakibulislam@gmail.com>
2021-03-07 18:56:59 +0600

    changed database to postgresql

--- django_coaching/settings.py
+++ django_coaching/settings.py
@@ -75,14 +75,26 @@ WSGI_APPLICATION = 'django_coaching.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coaching',
+        'USER': 'shakibulislam',
+        'PASSWORD': 'Shakil121',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
